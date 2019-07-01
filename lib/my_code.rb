773f1e84f529a20f3545cc6549fd3fe1504b67cd
Patrick Harrison <jahman@bellatlantic.net>
2019-07-01 17:18:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,17 +1,65 @@
-do 
-  map_to_negativize [1,2,3,4,5,6][-1,-2,-3,-4,-5,-6]
-  end
-do 
-  map_to_no-chamge[peter,paul,john][peter,paul,john]
+def map_to_negativize(array)
+   # array.map { |n| n * -1} 
+   new_array=[]
+   counter=0
+   while counter < array.length do
+      new_array << array[counter] *-1
+      counter += 1
+    end
+    return new_array
 end
-do 
-  map_to_double[2,4,6,8][4,8,12,16]
-end
-map_to_square[1,2,3,4][1,2,9,16]
-reduce_to _total[4,5,6](15)
-source_array[4,5,6]
-starting_point=50
-reduce_to_total[4,5,6,50][65]
 
-reduce_to_all_true[2,4,true][]
-reduce_to_any_true[false,nil,true]
+def map_to_no_change(array) 
+  new_array=[]
+  counter=0
+  while counter < array.length do
+    new_array << array[counter] 
+    counter += 1
+  end
+  return new_array
+end
+
+def map_to_double(array)
+  new_array=[]
+  counter=0
+  while counter < array.length do
+    new_array << array[counter] *2
+    counter += 1
+  end 
+  return new_array
+end
+
+
+  return new_array
+end
+
+def reduce_to_total(array,starting_point=0)
+  new =starting_point
+  counter=0 
+  while  counter < array.length do
+    new += array[counter]    
+    counter +=1
+  end
+  return new
+end
+
+def reduce_to_all_true(array)
+  counter=0
+  while counter < array.length do
+     if array[counter] == false
+      return false
+     end
+      counter += 1
+  end 
+  true
+end
+def reduce_to_any_true(array)
+  counter=0
+  while counter < array.length do
+     if array[counter] == true
+      return true
+     end
+      counter += 1
+  end 
+  false
+end
